5cbb1dcaaaeb83d71c043ce547286ef6b1aaf65b
rajanikanthy <rajanikanthy@outlook.com>
2019-10-06 10:35:36 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import InMemoryCache from 'apollo-cache-inmemory';
+import HttpLink from 'apollo-link-http';
+import LiftDashboard from './components/dashboards/lifts/dashboard';
+
+
+// const cache = new InMemoryCache();
+// const link = new HttpLink({
+//   uri: 'http://localhost:3000/'
+// })
+
+
+// const client = new ApolloClient({
+//   cache,
+//   link
+// });
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>SnowTooth Mountain Resort</h2>
+      </div>
+      <LiftDashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/dashboards/lifts/dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LiftDashboard = () => {
+    return(<div>
+        <h2>Lift Dashboard</h2>
+    </div>)
+}
+
+export default LiftDashboard;
