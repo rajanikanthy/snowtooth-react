bda4d29b5f18ea8ded09bebbda33d27300b29d49
Kantha Reddy Yammanuru Rajani <rajanikanthy@Kanthas-MacBook-Pro.local>
2019-10-13 07:31:59 -0700

    Fix merge conflicts

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,5 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-<<<<<<< HEAD
   ReactDOM.unmountComponentAtNode(div);
-=======
->>>>>>> 5cbb1dcaaaeb83d71c043ce547286ef6b1aaf65b
 });
